1dd28c13861068b7c772f63c233c8a0643d13820
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-12 21:21:24 +0300

    task 13.1.4

--- SkillFactoryCSharp13Collections/Program.cs
+++ SkillFactoryCSharp13Collections/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new[] { 3, 7, 1, -9, 5 };
+        int[] arr2 = new[] { 1, 2, 3, 4, 5, 6, 9 };
+
+        Console.WriteLine(IsSorted(arr).ToString());
+        Console.WriteLine(IsSorted(arr2).ToString());
+        Console.ReadKey();
+    }
+
+    static bool IsSorted(int[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (!(array[i] <= array[i + 1]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
