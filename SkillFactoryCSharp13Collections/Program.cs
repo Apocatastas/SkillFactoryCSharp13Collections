3e717ae5a201fc5e065290631071335112a34dc7
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-17 21:30:17 +0300

    task 13.3.5

--- SkillFactoryCSharp13Collections/Program.cs
+++ SkillFactoryCSharp13Collections/Program.cs
@@ -1,40 +1,35 @@
 ﻿using System;
 using System.Collections;
 using System.Text;
-using SkillFactoryCSharp13Collections;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var phoneBook = new List<Contact>();
-        phoneBook.Add(new Contact("Игорь", 79990000000, "igor@example.com"));
-        phoneBook.Add(new Contact("Андрей", 79990000001, "andrew@example.com"));
-
-        AddUnique(new Contact("Игорь", 79990066601, "igor2@example.com"),phoneBook);
-
-        foreach (var contact in phoneBook)
+        var months = new List<string>()
         {
-            Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
-        }
+            "Jan", "Feb", "Mar", "Apr", "May"
+        };
 
+        var missing = new ArrayList()
+        {
+            1, 2, 3, 5, "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        };
+
+        GetMissing(months, missing);
+
+        foreach (var month in months)
+        {
+            Console.WriteLine(month);
+        }
+            
         Console.ReadKey();
     }
 
-    private static void AddUnique(Contact newContact, List<Contact> phoneBook)
+    private static void GetMissing(List<string> months, ArrayList missing)
     {
-        bool alreadyExists = false;
-        foreach (var contact in phoneBook)
-        {
-            if (contact.Name == newContact.Name)
-            {
-                alreadyExists = true;
-                break;
-            }
-        }
-
-        if (!alreadyExists)
-            phoneBook.Add(newContact);
-        phoneBook.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+        var missedArray = new string[7];
+        missing.GetRange(4, 7).CopyTo(missedArray);
+        months.AddRange(missedArray);
     }
 }
