12c0b849fad9b396bd3f16a59faffb68a2c1388e
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-17 21:44:32 +0300

    task 13.3.12

--- SkillFactoryCSharp13Collections/Program.cs
+++ SkillFactoryCSharp13Collections/Program.cs
@@ -4,19 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        string sentence = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
-        var characters = sentence.ToCharArray();
-        var symbols = new HashSet<char>();
-        foreach (var symbol in characters)
-            symbols.Add(symbol);
-        Console.WriteLine(symbols.Count);
+        // Запускаем бесконечный цикл
+        while (true)
+        {
+            Console.WriteLine("Введите текст:");
+            var sentence = Console.ReadLine();
+            var characters = sentence.ToCharArray();
+            var symbols = new HashSet<char>();
 
-        var signs = new[] { ',', ' ', '.' };
-        var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        bool containsNumbers = symbols.Overlaps(numbers);
-        Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
+            foreach (var symbol in characters)
+                symbols.Add(symbol);
+            Console.WriteLine($"Всего {symbols.Count} уникальных символов");
 
-        symbols.ExceptWith(signs);
-        Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
+            var signs = new[] { ',', ' ', '.' };
+            var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            bool containsNumbers = symbols.Overlaps(numbers);
+            Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
+
+            symbols.ExceptWith(signs);
+            Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
+            Console.WriteLine();
+        }
     }
 }
