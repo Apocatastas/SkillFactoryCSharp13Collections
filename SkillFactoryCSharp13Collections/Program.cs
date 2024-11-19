963f8facdece9f9d797588cd768f1539b534a2f1
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-19 21:16:29 +0300

    task 13.4.5 - unsorted dictionary result 0.7963 ms

--- SkillFactoryCSharp13Collections/Program.cs
+++ SkillFactoryCSharp13Collections/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using SkillFactoryCSharp13Collections;
+using System.Diagnostics;
 
 class Program
 {
@@ -9,20 +10,13 @@ class Program
         ["Mike"] = new Contact(12345678901, "Mike@rolling-stones.com"),
     };
 
-    /// <summary>
-    /// Программа при запуске  должна :
-    /// 1. Вывести существующие контакты с номерами телефонов(на старте должно храниться 2).
-    /// 2. Добавить новый контакт.
-    /// 3. Вывести обновлённый список.
-    /// 4. Изменить у вновь добавленного контакта номер телефона.
-    /// 5. Снова вывести весь список.
-    /// </summary>
-    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Текущий список контактов: ");
         PrintContacts();
+        var watchTwo = Stopwatch.StartNew();
         PhoneBook.TryAdd("Lemmy", new Contact(12345678902, "lemmy@motorhead.com"));
+        Console.WriteLine($"Вставка в  словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
         Console.WriteLine("Обновленный список контактов: ");
         PrintContacts();
         if (PhoneBook.TryGetValue("Lemmy", out Contact contact))
